24ce6f8964db116764df9fff95fb6a1f7be3e361
Leonardo Asciuti Carmagnani <lasciuti.c@gmail.com>
2024-08-30 10:55:46 -0300

    Refact: Font color bug

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from "./Contexts/darkthemecontext";
 export default function App() {
 
   return <>
-  <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+  <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
    { <Home/>}
   </ThemeProvider>
   </>
